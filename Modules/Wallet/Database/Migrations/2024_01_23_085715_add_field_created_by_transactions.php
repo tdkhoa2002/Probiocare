45b646687996a610ad5f7e075e1560171782cd74
Dang Khoa <tdkhoa1101@gmail.com>
2024-01-24 15:22:56 +0700

    Update add table

--- Modules/Wallet/Database/Migrations/2024_01_23_085715_add_field_created_by_transactions.php
+++ Modules/Wallet/Database/Migrations/2024_01_23_085715_add_field_created_by_transactions.php
@@ -26,7 +26,7 @@ class AddFieldCreatedByTransactions extends Migration
     public function down()
     {
         Schema::table('wallet__transactions', function (Blueprint $table) {
-            $table->dropColumn('is_sync');
+            $table->dropColumn('created_by');
         });
     }
 }
